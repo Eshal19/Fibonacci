afb534d84d054667442707c224053037b6652167
Eshal19 <98449428+Eshal19@users.noreply.github.com>
2022-04-08 17:09:30 +0530

    Add files via upload

---
+++ Fibonacci series bot.py
@@ -0,0 +1,33 @@
+print("Do you want to learn the fibonacci series with fun?,Type 'yes' to start")
+
+hi=input("Enter yes to start here: ")
+
+if (hi=='yes'):
+    print("Hi! Lets start with 1 to 10")
+
+print("Basically in this series of numbers all you have to do is add the number like 0 + 1 = 1 and 1 + 2 = 3 and 3 + 4 is 7 , so the numbers 1,3,7 make up the fibonacci series . Addition is ofcourse ve-ry must so hang on and put on your seat belts for a succesful journey!")
+
+print("if i ask you want is 7 + 5 , what will be your answer?")
+sevenandfive=int(input("Enter your answer here: "))
+
+if (sevenandfive==12):
+    print('Perfect!')
+
+else :
+    print("No worries! i'll get you fantastic at math! , it's actually 12")
+
+def add(a,b):
+    print(a+b)
+
+print('Okay so what is 3+5')
+inp1=input("Enter your answer here:")
+if (inp1=='8'):
+    print ("correct!")
+    
+else:
+     ('almost correct! well done mayday!the answer is 8')
+
+enter=(input('Enter "enter" if its clear!'))
+print('Congratulations! now you can do it from 1 to 10')
+print("0 + 1 is 1 and 1 plus 2 is 3!")
+
